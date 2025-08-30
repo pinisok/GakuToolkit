03488a339f9d6269eb75102a285c2417bf3d7ce8
pinisok <prain3ps@gmail.com>
2025-08-30 17:52:18 +0900

    support adding new handler

--- scripts/log.py
+++ scripts/log.py
@@ -4,7 +4,7 @@ from rich.logging import RichHandler, Console
 
 FORMAT = "%(message)s"
 logging.basicConfig(format=FORMAT, datefmt="[%X]", handlers=[RichHandler(console=Console(stderr=True))])
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("GakuToolkit")
 print(f"Create logger {logger.name}")
 
 def LOG_DEBUG(depth, msg, *args, **kwargs):
@@ -22,3 +22,6 @@ def LOG_WARN(depth, msg, *args, **kwargs):
 def LOG_ERROR(depth, msg, *args, **kwargs):
     if logger.isEnabledFor(logging.ERROR):
         logger._log(logging.ERROR, "\t"*depth + msg, args, kwargs)
+
+def AddLogHandler(handler):
+    logger.addHandler(handler)
